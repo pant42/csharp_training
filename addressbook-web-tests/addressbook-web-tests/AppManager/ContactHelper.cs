50818eff7a22c0d2498eb35ac1194689461a897f
Alexander Pantin <pantin_42@inbox.ru>
2025-02-08 18:20:43 +0300

    Лекция 3.1. Оптимизация ввода данных в формы. GroupHelper FillGroupForm + ContactHelper FillContactForm

--- addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
@@ -27,13 +27,8 @@ namespace WebAddressbookTests
         }
         public ContactHelper FillContactForm(ContactData contact)
         {
-            driver.FindElement(By.Name("firstname")).Click();
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name("lastname")).Click();
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
-
+            Type(By.Name("firstname"), contact.Firstname);
+            Type(By.Name("lastname"), contact.Lastname);        
             return this;
         }
         public ContactHelper SubmitContactCreation()
--- addressbook-web-tests/addressbook-web-tests/AppManager/HelperBase.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/HelperBase.cs
@@ -8,10 +8,21 @@ namespace WebAddressbookTests
         protected IWebDriver driver;
         protected ApplicationManager manager;
 
-        public HelperBase (ApplicationManager manager)
+        public HelperBase(ApplicationManager manager)
         {
             this.manager = manager;
             driver = manager.Driver;
-        }    
+        }
+
+        public void Type(By locator, string text)
+        {
+            if (text != null)
+            {
+                driver.FindElement(locator).Clear();
+                driver.FindElement(locator).SendKeys(text);
+            }
+
+
+        }
     }
 }
